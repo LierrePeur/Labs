608d61fe5c93f5db17f43f1b5cf7be92d2c193e1
Danil Kushch <73854173+Reap3r@users.noreply.github.com>
2020-11-13 16:55:50 +0500

    Update 2_4.cpp

--- 2_4.cpp
+++ 2_4.cpp
@@ -2,10 +2,10 @@
 using namespace std;
 
 int main() {
-	int n, j;
+	int n;					
 	cin >> n;
 	for(int i=n; i>=1;i-=2){
-		for(j=1;j<=i/2;j++){
+		for(int j=1;j<=i/2;j++){
 			cout << " ";
 		}
 		for(int k=1;k<=(n+1)-i;k++){
